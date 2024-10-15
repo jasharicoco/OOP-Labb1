4e848bf6f8a2d2913309a50e678e6ecb9948b6d0
jasharicoco <jashari.egzon@hotmail.com>
2024-10-15 12:24:28 +0200

    added a "Triangle" class for educational purposes!! (for myself)

--- OOP Labb1/Program.cs
+++ OOP Labb1/Program.cs
@@ -4,14 +4,23 @@
     {
         static void Main(string[] args)
         {
-            //Skapa en cirkel med radie 5 cm
+            //Skapa cirklar med olika radie
             Circle circle1 = new Circle(5);
             Circle circle2 = new Circle(10);
 
             circle1.GetArea();
             circle1.GetVolume();
+            Console.WriteLine("");
             circle2.GetArea();
             circle2.GetVolume();
+
+            //Skapa trianglar med olika baser och höjder
+            Triangle triangle1 = new Triangle(2, 5);
+            Triangle triangle2 = new Triangle(4, 10);
+
+            Console.WriteLine("");
+            triangle1.GetArea();
+            triangle2.GetArea();
         }
     }
 }
